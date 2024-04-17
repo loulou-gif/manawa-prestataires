af656063d9881b39b0e405c0e46c042dbfbc54d8
unknown <konankanjulius10@gmail.com>
2024-04-17 14:21:11 +0000

    parametre

--- pages/forms/ModifyServices.js
+++ pages/forms/ModifyServices.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Header from '../../components/customers/Header'
 
 const ModifyServices = () => {
   return (
     <View>
-      <Text>ModifyServices</Text>
+      <Header/>
     </View>
   )
 }
